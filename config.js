59fe383f8d7407b79666bf53cdb9136842d0fa38
Charles Amoako <charles.amoako.128@gmail.com>
2017-01-04 12:14:14 -0500

    set config

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  server: {},
+  server: {
+    databaseURI: '8421975bdabd4486b693d10ca0f7d261/iad1-c18-0.mongo.objectrocket.com:51241,iad1-c18-2.mongo.objectrocket.com:51241,iad1-c18-1.mongo.objectrocket.com:51241/altgabetest', // Connection string for your MongoDB database
+    cloud: './cloud/main.js', // Absolute path to your Cloud Code
+    appId: 'ojQS3VbZhl13BOcG4CMIsHKr2xZvz1kg0tdvqQJq',
+    masterKey: 'BndVAXUAXeFE4XxXFOfEm1Be4FgbSU19cTVUq6to',
+    serverURL: 'https://altcharles.azurewebsites.net:1337/parse'
+  },
   dashboard: {},
   storage: {},
   push: {}
-}
+};
--- index.js
+++ index.js
@@ -8,11 +8,16 @@ var config = parseServerConfig(__dirname);
 
 // Modify config as necessary before initializing parse server & dashboard
 
+
 var app = express();
+
+app.get('/', (req, res) => {
+  res.send('HOME');
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/parse', new ParseServer(config.server));
-app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
-
+// app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
 app.listen(process.env.PORT || url.parse(config.server.serverURL).port, function () {
   console.log(`Parse Server running at ${config.server.serverURL}`);
 });
